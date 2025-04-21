0904882330e9a1755a78688aeddb117375b08912
lordkuyq <nikitinda@302D-15>
2025-04-21 09:05:52 +0700

    0.0.0.9

--- script_page7.js
+++ script_page7.js
@@ -88,4 +88,148 @@ document.addEventListener('DOMContentLoaded', function(arr) {
                 timer2.stop();
             }
         });
+
+        // t3
+        function delay(N) {
+            return new Promise((resolve) => {
+                setTimeout(resolve, N*1000);
+            });
+        }
+        
+        document.getElementById('start-t3').addEventListener('click', function() {
+            console.log("click");
+
+            n = document.getElementById('input-t3').value;
+
+            delay(n).then(() => {
+                document.getElementById('answer-t3').value = `Прошло ${n} секунд`;
+            });
+            });
+
+        // t4
+        
+        document.getElementById('start-t4').addEventListener('click', function() {
+            console.log("click");
+
+            n = document.getElementById('input-t4').value;
+
+            function updN(n) {
+                if (n < 0) return;
+        
+                document.getElementById('answer-t4').value = `Счетчик равен ${n}`;
+                delay(1).then(() => {
+                    updN(n - 1);
+                });
+            }
+        
+            updN(n);
+            });
+
+        // t5
+
+        class HttpError extends Error {
+            constructor(response) {
+              super(`${response.status} for ${response.url}`);
+              this.name = 'HttpError';
+              this.response = response;
+            }
+          }
+          
+          function loadJson(url) {
+            return fetch(url)
+              .then(response => {
+                if (response.status == 200) {
+                  return response.json();
+                } else {
+                  throw new HttpError(response);
+                }
+              })
+          }
+          
+          function getGithubUser() {
+            let name = document.getElementById('input-t5').value;
+          
+            return loadJson(`https://api.github.com/users/${name}`)
+              .then(user => {
+                document.getElementById('answer-t5').value = user.repos_url;
+                console.log("log1");
+                return user.repos_url;
+              })
+              .catch(err => {
+                if (err instanceof HttpError && err.response.status == 404) {
+                  document.getElementById('answer-t5').value = "Такого пользователя нет";
+                  return demoGithubUser();
+                } else {
+                  throw err;
+                }
+              });
+          }
+
+          function getGithubRepos(reposUrl) {
+            return loadJson(reposUrl)
+              .then(repos => {
+                if (repos.length > 0) {
+                    document.getElementById('answer-2-t5').value = `Реп-ий: ${repos[0].full_name}.`;
+                  } 
+                else {
+                    document.getElementById('answer-2-t5').value = "Нет";
+                  }
+                return repo;
+              })
+              .catch(err => {
+                if (err instanceof HttpError && err.response.status == 404) {
+                  document.getElementById('answer-2-t5').value = "Такого json нет";
+                  return demoGithubUser();
+                } else {
+                  throw err;
+                }
+              });
+          }
+          
+          
+          document.getElementById('start-t5').addEventListener('click', function() {
+            console.log("click");
+            getGithubUser()
+                .then(reposUrl => {
+                return getGithubRepos(reposUrl);
+                })
+                .catch(err => {
+                console.error("Error:", err);
+                });
+            });
+
+        // t6
+
+
+          async function loadJson_t6(url) {
+            const response = await fetch(url);
+            if (response.status == 200) {
+                return response.json();
+            } else {
+                throw new HttpError(response);
+            }
+        }
+          
+          async function getGithubUser_t6() {
+            let name = prompt("Введите логин?", "lordkuyq");
+          
+            const response = await loadJson_t6(`https://api.github.com/users/${name}`);
+              try{
+                console.log(response);
+                  alert(`Полное имя: ${response.login}.`);
+                }
+              catch(err) {
+                if (err instanceof HttpError && err.response.status == 404) {
+                  alert("Такого пользователя не существует, пожалуйста, повторите ввод.");
+                  return demoGithubUser();
+                } else {
+                  throw err;
+                }
+              }
+          }
+
+          document.getElementById('start-t6').addEventListener('click', function() {
+              getGithubUser_t6();
+            });
+            
 });
